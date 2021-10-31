f245c0fa0c124726fdc2ed301c79d21f7c19e60e
indieg21 <Indiagraham21@gmail.com>
2021-10-30 23:32:25 -0400

    Adding to problem 6

--- PROJECT 6/script.js
+++ PROJECT 6/script.js
@@ -1,19 +1,10 @@
+let answer= [ "C. New York City", "B. Paris", "B. Five" ]
+function check(){
+    //console.log("Hello World")
+    let answer= document.querySelectorAll(".answer");
+    answer.map((ans)=>{
+        console.log(ans.vaule)
 
-var c=0;
-    var q1=document.quiz.question1.vaule;
-    var q2=document.quiz.question2.vaule;
-    var q3=document.quiz.question3.vaule;
-    var answer=document.getElementById("answer")
-    if (q1=="Javascript") {c++; alert("Correct! C. New York City")}
-    else {
-        alert("Wrong! Correct answer is C. New York City")
-    }
-    if (q2=="Javascript") {c++}
-    if (q3=="Javascript") {c++}
-
-
+    })
     
-    
-    
-    
-    
+}
